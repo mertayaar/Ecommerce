10fe91a16d97ad726c49c256996ae27415825db5
mertayaar <mertayaar@gmail.com>
2025-10-10 21:09:38 -0700

    add category dtos

---
+++ Services/Catalog/Ecommerce.Catalog/Dtos/CategoryDtos/GetByIDCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Catalog.Dtos.CategoryDtos
+{
+    public class GetByIDCategoryDto
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
