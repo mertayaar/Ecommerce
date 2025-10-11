e9cc98327a289ac6e7192351ac085a9eb9414cda
mertayaar <mertayaar@gmail.com>
2025-10-11 09:44:59 -0700

    Add coupondtos

---
+++ Discount/Ecommerce.Discount/Dtos/CreateCouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Discount.Dtos
+{
+    public class CreateCouponDto
+    {
+        public string CouponCode { get; set; }
+        public int CouponRate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
---
+++ Discount/Ecommerce.Discount/Dtos/GetByIDCouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Discount.Dtos
+{
+    public class GetByIDCouponDto
+    {
+        public int CouponID { get; set; }
+        public string CouponCode { get; set; }
+        public int CouponRate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
