d00b056a5d8fa34e5beeed653803b9aa95878ac1
mertayaar <mertayaar@gmail.com>
2025-10-10 22:27:29 -0700

    add ProductDetailsController

---
+++ Services/Catalog/Ecommerce.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,55 @@
+﻿using Ecommerce.Catalog.Dtos.ProductDetailDtos;
+using Ecommerce.Catalog.Services.ProductDetailServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _ProductDetailService;
+
+        public ProductDetailsController(IProductDetailService ProductDetailService)
+        {
+            _ProductDetailService = ProductDetailService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailList()
+        {
+            var values = await _ProductDetailService.GetAllProductDetailAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetailByID(string id)
+        {
+            var value = await _ProductDetailService.GetByIDProductDetailAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+        {
+            await _ProductDetailService.CreateProductDetailAsync(createProductDetailDto);
+            return Ok(createProductDetailDto);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetail(string id)
+        {
+            await _ProductDetailService.DeleteProductDetailAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            await _ProductDetailService.UpdateProductDetailAsync(updateProductDetailDto);
+            return Ok();
+        }
+    }
+}
+}
