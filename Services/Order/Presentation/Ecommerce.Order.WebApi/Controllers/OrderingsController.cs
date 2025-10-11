d4df15ea06abd6db6572fadfdb1c9e3c10dc2e45
mertayaar <mertayaar@gmail.com>
2025-10-11 14:19:52 -0700

    mediator registration

---
+++ Services/Order/Presentation/Ecommerce.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,57 @@
+﻿using Ecommerce.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using Ecommerce.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using Ecommerce.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var values = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderDetailCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderDetailCommand(id));
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
--- Services/Order/Presentation/Ecommerce.Order.WebApi/Program.cs
+++ Services/Order/Presentation/Ecommerce.Order.WebApi/Program.cs
@@ -1,9 +1,17 @@
 using Ecommerce.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using Ecommerce.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using Ecommerce.Order.Application.Interfaces;
+using Ecommerce.Order.Application.Services;
+using Ecommerce.Order.Persistance.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+
+#region
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -15,7 +23,7 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
-
+#endregion
 // Add services to the container.
 
 builder.Services.AddControllers();
