d00a0a10410aab2bffbf03ef941f221c535f6d10
mertayaar <mertayaar@gmail.com>
2025-10-10 22:37:49 -0700

    programcs configuration

--- Services/Catalog/Ecommerce.Catalog/Program.cs
+++ Services/Catalog/Ecommerce.Catalog/Program.cs
@@ -1,6 +1,26 @@
+using Ecommerce.Catalog.Services.CategoryServices;
+using Ecommerce.Catalog.Services.ProductDetailServices;
+using Ecommerce.Catalog.Services.ProductImageServices;
+using Ecommerce.Catalog.Services.ProductServices;
+using Ecommerce.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
