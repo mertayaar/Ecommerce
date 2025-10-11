d97833f1579adbd3d48a8c2094adbc709edc0745
mertayaar <mertayaar@gmail.com>
2025-10-10 20:44:38 -0700

    confg appsetting and add entities

---
+++ Services/Catalog/Ecommerce.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ecommerce.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInformation { get; set; }
+
+    }
+}
---
+++ Services/Catalog/Ecommerce.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Ecommerce.Catalog.Entities
+{
+    public class ProductImages
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductImagesID { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string ProductID { get; set; }
+        public Product Product { get; set; }    
+        
+
+
+    }
+}
