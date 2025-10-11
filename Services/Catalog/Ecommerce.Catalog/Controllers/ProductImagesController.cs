0f094eb1b6c2fcc2e971023e807e8b94fb5d79d9
mertayaar <mertayaar@gmail.com>
2025-10-10 22:29:05 -0700

    add ProductImagesController

---
+++ Services/Catalog/Ecommerce.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,55 @@
+﻿using Ecommerce.Catalog.Dtos.ProductImageDtos;
+using Ecommerce.Catalog.Services.ProductImageServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageByID(string id)
+        {
+            var value = await _ProductImageService.GetByIDProductImageAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok(createProductImageDto);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok();
+        }
+    }
+}
+}
