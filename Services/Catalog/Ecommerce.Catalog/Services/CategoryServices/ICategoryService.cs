4efdad3acde78bc2bd0294c5794b3eb3bd1661de
mertayaar <mertayaar@gmail.com>
2025-10-10 21:50:02 -0700

    add CategoryService

---
+++ Services/Catalog/Ecommerce.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Catalog.Dtos.CategoryDtos;
+
+namespace Ecommerce.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+
+        Task<GetByIDCategoryDto> GetByIDCategoryAsync(string id);
+    }
+}
