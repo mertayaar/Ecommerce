93dcb9d4c1229026aa9f2d01c5fc1c03d34736e1
mertayaar <mertayaar@gmail.com>
2025-10-10 21:28:00 -0700

    add automapper

--- Services/Catalog/Ecommerce.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/Ecommerce.Catalog/Entities/ProductImage.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Ecommerce.Catalog.Entities
 {
-    public class ProductImages
+    public class ProductImage
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
---
+++ Services/Catalog/Ecommerce.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Ecommerce.Catalog.Dtos.CategoryDtos;
+using Ecommerce.Catalog.Dtos.ProductDetailDtos;
+using Ecommerce.Catalog.Dtos.ProductDtos;
+using Ecommerce.Catalog.Dtos.ProductImageDtos;
+using Ecommerce.Catalog.Entities;
+
+namespace Ecommerce.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIDCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIDProductDto>().ReverseMap();
+
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIDProductDetailDto>().ReverseMap();
+
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, GetByIDProductImageDto>().ReverseMap();
+        }
+    }
+}
