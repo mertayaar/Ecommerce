faa7e83c0b2d6dd6e9daafb453be461a5279ba30
mertayaar <mertayaar@gmail.com>
2025-10-11 00:23:55 -0700

    Add coupon entities and db settings

---
+++ Discount/Ecommerce.Discount/Context/DapperContext.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Ecommerce.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Discount/Ecommerce.Discount/Entities/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Discount.Entities
+{
+    public class Coupon
+    {
+        public int CouponID { get; set; }
+        public string CouponCode { get; set; }
+        public int CouponRate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
