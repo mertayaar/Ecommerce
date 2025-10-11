4c549d4d46e848b17baf5ef8a01a3a6fa52df016
mertayaar <mertayaar@gmail.com>
2025-10-11 12:28:36 -0700

    add repository interface

---
+++ Services/Order/Infrastructure/Ecommerce.Order.Persistance/Context/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Order.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Order.Persistance.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+
+    }
+}
