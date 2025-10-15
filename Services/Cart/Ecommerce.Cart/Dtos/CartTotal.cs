2c01c8f4be5116c9c78b5ebea994f4a56a274ae6
mertayaar <mertayaar@gmail.com>
2025-10-14 18:17:24 -0700

    add redis configuration

---
+++ Services/Cart/Ecommerce.Cart/Dtos/CartTotal.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Cart.Dtos
+{
+    public class CartTotal
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        public List<CartItemDto> CartItems { get; set; }
+        public decimal TotalPrice { get => CartItems.Sum(x=>x.Price * x.Quantity);}
+    }
+}
