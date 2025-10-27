3806887bafbaec4fe72468baf279453fd5d65191
mertayaar <mertayaar@gmail.com>
2025-10-27 15:55:02 -0700

    Add SpecialOffer DTO, MVC and API controller
    - Added SpecialOfferDto
    - Implemented SpecialOffer MVC controller
    - Implemented SpecialOffer API controller

---
+++ Services/Catalog/Ecommerce.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,55 @@
+﻿using Ecommerce.Catalog.Dtos.SpecialOfferDtos;
+using Ecommerce.Catalog.Services.SpecialOfferServices;
+using Ecommerce.Catalog.Services.SpecialOfferServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var values = await _specialOfferService.GetAllSpecialOfferAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialOfferById(string id)
+        {
+            var value = await _specialOfferService.GetByIdSpecialOfferAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDto);
+            return Ok(createSpecialOfferDto);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDto);
+            return Ok();
+        }
+    }
+}
---
+++ Services/Catalog/Ecommerce.Catalog/Services/SpecialOfferServices/ISpecialOfferService.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Catalog.Dtos.SpecialOfferDtos;
+
+namespace Ecommerce.Catalog.Services.SpecialOfferServices
+{
+    public interface ISpecialOfferService
+    {
+        Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync();
+        Task CreateSpecialOfferAsync(CreateSpecialOfferDto createSpecialOfferDto);
+        Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto updateSpecialOfferDto);
+        Task DeleteSpecialOfferAsync(string id);
+
+        Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id);
+    }
+}
