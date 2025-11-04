042de4be8480f466922ddd423d3bcb9afba2c75a
mertayaar <mertayaar@gmail.com>
2025-11-04 19:15:44 +0300

    add register page

---
+++ frontends/Ecommerce.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
