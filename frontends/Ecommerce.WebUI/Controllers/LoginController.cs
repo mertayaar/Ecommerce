904e34345fe057a0530ca0d2f974d08a33a8eb55
mertayaar <mertayaar@gmail.com>
2025-11-05 14:03:05 +0300

    add login and register controller, index

---
+++ frontends/Ecommerce.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
